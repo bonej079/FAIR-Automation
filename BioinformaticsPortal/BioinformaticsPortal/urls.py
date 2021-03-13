873b2251b06f0f2fcee856fc0b5bec7fdd596c14
Jurgen Aquilina <jurgenaquilina.12@gmail.com>
2021-03-13 13:01:38 +0100

    Removed unnecessary routes and form

--- BioinformaticsPortal/BioinformaticsPortal/urls.py
+++ BioinformaticsPortal/BioinformaticsPortal/urls.py
@@ -19,7 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('portal.urls')),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('users/', include('django.contrib.auth.urls')),
     path('accounts/', include('allauth.urls')),
 ]
--- BioinformaticsPortal/users/views.py
+++
@@ -1,9 +0,0 @@
-from django.urls import reverse_lazy
-from django.views import generic
-
-from .forms import AdminPortalUserCreationForm
-
-class SignUp(generic.CreateView):
-    form_class = AdminPortalUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'users/signup.html'
